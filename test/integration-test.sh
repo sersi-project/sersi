0c5fe559bf68b825577aeeea2dc879ba45cbddde
KristienN <knyamutsaka@outlook.com>
2025-06-12 22:18:19 +0100

    refactor pieplien

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -2,3 +2,7 @@
 
 # TODO: Implement integration tests
 echo "Integration tests not implemented yet"
+
+# Test scaffold
+chmod +x ./bin/sersi
+./bin/sersi frontend -n integration-test -f react -c tailwind -l js
